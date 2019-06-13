1ef28e087918d80a39017ad1b3483722339d430a
klongthomhospital <klongthomhospital@gmail.com>
2019-06-13 15:45:29 +0700

    Learn Vue ability

--- kthos/pages/vue-ability.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>ability</h1>
-    </div>
-</template>
---
+++ kthos/pages/vue-ability/01-data-binding.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="con-center">
+        <h1>01.Data-Binding</h1>
+        <br><hr>
+        <h1>Hello {{name}}</h1>
+        <input type="text" :value="name" />
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name : "Nont"
+        }
+    }
+}
+</script>
+
+<style>
+ .con-center{
+     text-align: center;
+     margin-top: 2rem;
+ }
+</style>
+
+
