a7930de8f4ca184eb647e180087be727429549ac
klongthomhospital <klongthomhospital@gmail.com>
2019-06-13 13:22:27 +0700

    Vue

---
+++ kthos/pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <div class="con-center">
+           <h1>About klongthom Hospital</h1>
+        </div>
+    </section>
+</template>
+
+
+
+<style>
+    .con-center{
+        text-align: center;
+        margin-top: 1rem;  
+    }
+
+</style>
+
